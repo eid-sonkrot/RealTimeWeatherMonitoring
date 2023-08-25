34f4f6aa0e0b38fc53837443038ea3d897c21f80
Smile <Smile@DESKTOP-NOGL6KQ>
2023-08-25 22:24:29 +0300

    Implemented Client-Side WeatherStation and User Interface with Observer Design Pattern
    This commit introduces the client-side components of the WeatherStation, including the WeatherStation class itself and a user interface. The Observer design pattern has been employed to establish a robust communication mechanism between the WeatherStation and its observers, ensuring that weather updates are efficiently propagated to registered entities.
    
    Changes Made:
    - Implemented the WeatherStation class to simulate weather data updates.
    - Created a basic user interface for users to interact with the WeatherStation.
    - Integrated the Observer design pattern to facilitate communication between the WeatherStation and observers.
    - Demonstrated the effectiveness of the Observer pattern by updating registered entities when weather conditions change.
    
    This commit significantly enhances the system's ability to monitor and report weather conditions, providing a user-friendly interface for interaction while leveraging the Observer design pattern for efficient data dissemination.

--- RealTimeWeatherMonitoring/RealTimeWeatherMonitoring.cs
+++ RealTimeWeatherMonitoring/RealTimeWeatherMonitoring.cs
@@ -1,9 +1,129 @@
-﻿namespace RealTimeWeatherMonitoring
+﻿using System.Text.Json;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using System.Xml;
+
+namespace RealTimeWeatherMonitoring
 {
     class RealTimeWeatherMonitoring
     {
+        
         public static void Main()
         {
+            var realProject = new RealTimeWeatherMonitoring();
+            var weatherStation = new WeatherStation();
+
+            new BotFactory().CreatBots();
+            realProject.SubscribeBot(ref weatherStation);
+            realProject.HellowMassge();
+            Thread.Sleep(1000);
+            while (true)
+            {
+                var choice=realProject.GetChoice();
+
+                Console.Clear();
+                switch (choice)
+                {
+                    case 1:
+                        realProject.UpdateWeatherData(ref realProject,ref weatherStation);
+                        break;
+                    case 2:
+                        Console.WriteLine("Exiting program...");
+                        return;
+                    default:
+                        Console.WriteLine("Invalid choice. Please select a valid option.");
+                        break;
+                }
+            }
+        }
+        public void UpdateWeatherData(ref RealTimeWeatherMonitoring realProject, ref WeatherStation weatherStation)
+        {
+            var Data = (string)(null);
+            var weatherData = new WeatherData();
+
+            Data =realProject.UserInput();
+            weatherData = realProject.GetWeatherData(Data);
+            if (weatherData == null)
+            {
+                realProject.WrongDataFormatteMassge();
+            }
+            weatherStation.UpdateWeatherData(weatherData);
+            realProject.PreesAnyKey();
+        }
+        public string? UserInput()
+        {
+            Console.Clear();
+            Console.WriteLine("Enter weather data : ");
+            var Data = Console.ReadLine();
+
+            Console.Clear();
+            return Data;
+        }
+        public void ChoiceOption()
+        {
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1. Update Weather Data");
+            Console.WriteLine("2. Exit Program");
+            Console.Write("Enter option : ");
+        }
+        public int GetChoice()
+        {
+            new RealTimeWeatherMonitoring().ChoiceOption();
+            var choice = int.Parse(Console.ReadLine());
+
+            return choice;
+        }
+        public void SubscribeBot(ref WeatherStation weatherStation)
+        {
+            weatherStation.Subscribe(SunBot.GetSunBot());
+            weatherStation.Subscribe(SnowBot.GetSnowBot());
+            weatherStation.Subscribe(RainBot.GetRainBot());
+        }
+        public void HellowMassge()
+        {
+            Console.WriteLine("welcome to Foothill station");
+        }
+        public void WrongDataFormatteMassge()
+        {
+            Console.WriteLine("Wrong Data input");
+            Thread.Sleep(1000);
+        }
+        public void PreesAnyKey()
+        {
+            Console.Write("Prees Any Key : ");
+            Console.ReadKey();
+        }
+        public WeatherData GetWeatherData(string Data)
+        {
+            try
+            {
+                var dataObject = new DataFormateFactory().CreateDataFormate(Data).GetDocument(Data);
+
+                if (dataObject.GetType() == typeof(JsonDocument))
+                {
+                    var jsonObject = (JsonDocument)dataObject;
+
+                    return jsonObject.Deserialize<WeatherData>();
+                }
+                if (dataObject.GetType() == typeof(XDocument))
+                {
+                    var xmlDoc = (XDocument)dataObject;
+                    var serializer = new XmlSerializer(typeof(WeatherData));
+                    var weatherData = new WeatherData();
+
+                    using (XmlReader reader = xmlDoc.CreateReader())
+                    {
+                        weatherData = (WeatherData)serializer.Deserialize(reader);
+                    }
+                    return weatherData;
+                }
+                return null;
+            }
+            catch (Exception? ex)
+            {
+                Console.WriteLine($"Invalid data format {ex.Message}");
+                return null;
+            }
         }
     }
 }
---
+++ RealTimeWeatherMonitoring/WeatherStation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealTimeWeatherMonitoring
+{
+    public class WeatherStation:IObservable
+    {
+        private List<IObserver> observers = new List<IObserver>();
+
+        public void Subscribe(IObserver observer)
+        {
+            observers.Add(observer);
+        }
+        public void Unsubscribe(IObserver observer)
+        {
+            observers.Remove(observer);
+        }
+        public void NotifyObservers(WeatherData weatherData)
+        {
+            observers.ForEach(observer => observer.Update(weatherData));
+        }
+        public void UpdateWeatherData(WeatherData weatherData)
+        {
+            NotifyObservers(weatherData);
+        }
+    }
+}
