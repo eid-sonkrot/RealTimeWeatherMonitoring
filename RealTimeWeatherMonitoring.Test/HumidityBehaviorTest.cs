6f32fdaf0303a30208457c54d981727d07291246
Smile <Smile@DESKTOP-NOGL6KQ>
2023-09-06 18:37:51 +0300

    [Refactor code ] in this commit i rewrite Test for bot behavior

--- RealTimeWeatherMonitoring.Test/HumidityBehaviorTest.cs
+++ RealTimeWeatherMonitoring.Test/HumidityBehaviorTest.cs
@@ -1,26 +1,62 @@
-﻿using Moq;
+﻿using AutoFixture;
+using Moq;
 
 namespace RealTimeWeatherMonitoring.Test
 {
     [TestClass]
     public class HumidityBehaviorTest
     {
+        private Fixture fixture;
+        private StringWriter sw;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            fixture = new Fixture();
+            sw = new StringWriter();
+        }
         [TestMethod]
-        public void Test_Humidity_Execute_Behavior()
+        public void Execute_ShouldPrintMessage_WhenHumidityIsAboveThreshold()
         {
             //Arange
             var weatherData = new WeatherData()
             {
-                Temperature = 90,
-                Humidity = 1000
+                Temperature = fixture.Create<double>(),
+                Humidity = fixture.Create<double>()
             };
-            var botBehaviorMock = new Mock<HumidityBehavior>();
-            var botBehavior = botBehaviorMock.Object;
-            var threshold = 50;
-            var message = "wewew";
+            var botBehavior = new HumidityBehavior();
+            var threshold = weatherData.Humidity - 1;
+            var message = fixture.Create<string>();
             //Act
-            //Assert
+            Console.SetOut(sw);
             botBehavior.Execute(message, threshold, weatherData);
+            var consoleOutput = sw.ToString().Trim();
+            //Assert
+            Assert.AreEqual(message, consoleOutput);
+        }
+        [TestMethod]
+        public void Execute_ShouldNotPrintMessage_WhenHumidityIsBelowThreshold()
+        {
+            //Arange
+            var weatherData = new WeatherData()
+            {
+                Temperature = fixture.Create<double>(),
+                Humidity = fixture.Create<double>()
+            };
+            var botBehavior = new HumidityBehavior();
+            var threshold = weatherData.Humidity + 1;
+            var message = fixture.Create<string>();
+            //Act
+            Console.SetOut(sw);
+            botBehavior.Execute(message, threshold, weatherData);
+            var consoleOutput = sw.ToString().Trim();
+            //Assert
+            Assert.AreEqual("", consoleOutput);
+        }
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            sw.Dispose();
         }
     }
 }
--- RealTimeWeatherMonitoring.Test/TemperatureBehaviorTest.cs
+++ RealTimeWeatherMonitoring.Test/TemperatureBehaviorTest.cs
@@ -1,26 +1,63 @@
-﻿using Moq;
+﻿using AutoFixture;
+using Moq;
 
 namespace RealTimeWeatherMonitoring.Test
 {
+     
     [TestClass]
     public class TemperatureBehaviorTest
     {
+        private Fixture fixture;
+        private StringWriter sw;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            fixture = new Fixture();
+            sw = new StringWriter();
+        }
         [TestMethod]
-        public void Test_Temperature_Execute_Behavior()
+        public void Execute_ShouldPrintMessage_WhenTemperatureIsAboveThreshold()
         {
             //Arange
             var weatherData = new WeatherData()
             {
-                Temperature = 90,
-                Humidity = 1000
+                Temperature = fixture.Create<double>(),
+                Humidity = fixture.Create<double>()
             };
-            var botBehaviorMock = new Mock<TemperatureBehavior>();
-            var botBehavior = botBehaviorMock.Object;
-            var threshold = 50;
-            var message = "wewew";
+            var botBehavior = new TemperatureBehavior();
+            var threshold = weatherData.Temperature-1;
+            var message = fixture.Create<string>();
             //Act
-            //Assert
+            Console.SetOut(sw);
             botBehavior.Execute(message, threshold, weatherData);
+            var consoleOutput = sw.ToString().Trim();
+            //Assert
+            Assert.AreEqual(message, consoleOutput);
+        }
+        [TestMethod]
+        public void Execute_ShouldNotPrintMessage_WhenTemperatureIsBelowThreshold()
+        {
+            //Arange
+            var weatherData = new WeatherData()
+            {
+                Temperature = fixture.Create<double>(),
+                Humidity = fixture.Create<double>()
+            };
+            var botBehavior = new TemperatureBehavior();
+            var threshold = weatherData.Temperature + 1;
+            var message = fixture.Create<string>();
+            //Act
+            Console.SetOut(sw);
+            botBehavior.Execute(message, threshold, weatherData);
+            var consoleOutput = sw.ToString().Trim();
+            //Assert
+            Assert.AreEqual("", consoleOutput);
+        }
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            sw.Dispose();
         }
     }
 }
